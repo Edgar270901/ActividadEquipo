b5d3bef2da03f3ab219ee430933f76fe06ee75e8
Carorms <qaroline.28@gmail.com>
2022-11-19 18:22:57 -0600

    deleted somethings and added some drawings

--- ColaLinealFutbolistas.java
+++ ColaLinealFutbolistas.java
@@ -1,48 +1,29 @@
+___
+o__        o__     |   |\
+/|          /\      |   |X\
+/ > o        <\     |   |XX\
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 public class ColaLinealFutbolistas {
     private Futbolista c[]; // Vector para manejar la fila de ENTEROS
     private int frente, fin; // Apuntadores para el frente y final de la estructura
 
-    public ColaLinealFutbolistas() {
-        c = new Futbolista[5];
-        frente = 0;
-        fin = -1;
-    }
-
-    public ColaLinealFutbolistas(int n) {
-        c = new Futbolista[n];
-        frente = 0;
-        fin = -1;
-    }
-
-    public void insertar(Futbolista x) {
-        if (!estaLlena()) {
-            fin++;
-            c[fin] = x;
-        }
-    }
-
-    public Futbolista eliminar() {
-        Futbolista borrado = null;
-        if (!estaVacia()) {
-            borrado = c[frente];
-            c[frente] = null;
-            frente++;
-            if (estaVacia()) { // si al eliminar, la cola queda VACÍA, se ajustan los
-                frente = 0; // apuntadores para reutilizar las casillas del principio
-                fin = -1; // y optimizar espacio
-            }
-        }
-        return borrado;
-    }
-
-    public boolean estaVacia() {
-        if (frente > fin) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
+   
     public boolean estaLlena() {
         if (fin == c.length - 1) {
             return true;
@@ -50,6 +31,8 @@ public class ColaLinealFutbolistas {
             return false;
         }
     }
+
+
 
     public int numElementos() {
         return fin - frente + 1;
